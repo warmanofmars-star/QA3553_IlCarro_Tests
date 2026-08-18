c5ea075d05b261040c992177a9db3b81ae8ff094
warmanofmars-star <warman.of.mars@gmail.com>
2026-08-18 19:14:47 +0300

    Ill Caro test 14.08.26

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,9 +1,13 @@
 from selenium.webdriver.common.by import By
 
+
 class LoginPage:
     EMAIL_INPUT = (By.CSS_SELECTOR, "input[name='username']")
     PASSWORD_INPUT = (By.CSS_SELECTOR, "input[name='password']")
     SUBMIT_BTN = (By.CSS_SELECTOR, "button[type='submit']")
+
+    # НОВЫЙ ЛОКАТОР: ищем заголовок h3 с точным текстом
+    SUCCESS_MSG = (By.XPATH, "//h3[text()='You are logged in success']")
 
     def __init__(self, driver):
         self.driver = driver
@@ -24,7 +28,6 @@ class LoginPage:
     def submit_login(self):
         self.driver.find_element(*self.SUBMIT_BTN).click()
 
-    # Helper
     def login(self, email, password):
         self.fill_email(email)
         self.fill_password(password)
@@ -33,3 +36,7 @@ class LoginPage:
     def is_submit_button_disabled(self):
         submit_btn = self.driver.find_element(*self.SUBMIT_BTN)
         return not submit_btn.is_enabled()
+
+    # НОВЫЙ МЕТОД: получаем текст из всплывающего окна
+    def get_success_message_text(self):
+        return self.driver.find_element(*self.SUCCESS_MSG).text
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,7 +1,5 @@
-import time
 from pages.login_page import LoginPage
 
-# Выносим реальные тестовые данные в константы
 VALID_EMAIL = "warman.of.mars@gmail.com"
 VALID_PASSWORD = "z2I8A@U!Hl3T2&@h"
 
@@ -15,10 +13,9 @@ def test_login_flow(driver):
     # 2. Убеждаемся, что кнопка заблокирована на пустой форме
     assert login_page.is_submit_button_disabled() == True, "Кнопка Y'alla! должна быть неактивна в начале"
 
-    # 3. Выполняем логин с нашими реальными данными через метод-помощник
+    # 3. Выполняем логин
     login_page.login(VALID_EMAIL, VALID_PASSWORD)
 
-    # Пауза, чтобы успеть визуально проконтролировать ввод данных и клик
-    time.sleep(2)
-
-    # В будущем добавим проверку, что мы оказались внутри
+    # 4. ФИНАЛЬНАЯ ПРОВЕРКА: ждем появления окна и проверяем его текст
+    actual_text = login_page.get_success_message_text()
+    assert actual_text == "You are logged in success", f"Ожидали текст успеха, а получили: {actual_text}"
