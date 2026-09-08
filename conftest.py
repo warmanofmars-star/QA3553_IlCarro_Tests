891aff13fc5cc343fc9addae5f774021cc242bc4
warmanofmars-star <warman.of.mars@gmail.com>
2026-09-08 20:40:16 +0300

    08.09.26

--- conftest.py
+++ conftest.py
@@ -16,6 +16,9 @@ def driver():
     if os.environ.get('CI') == 'true' or is_headless:
         options.add_argument('--headless')
         options.add_argument('--window-size=1920,1080')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--disable-gpu')
 
     driver = webdriver.Edge(options=options)
 
